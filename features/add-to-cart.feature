1a284455ffd23b1e14f5b1fe45d1fc1a72ff8c9c
Johannes <johanneskamph@gmail.com>
2018-04-26 11:48:55 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ features/add-to-cart.feature
@@ -0,0 +1,9 @@
+Feature: Add to shopping cart
+	As a user of Systembolagets online shop In order to buy beverages I want to be able to add a certain quantity of a beverage to my shopping cart
+
+
+    Scenario: Add a beverage in a shopping cart
+        Given that I'm in Systembolaget
+        When I have decided what I want to buy
+        And I have decided how much of a certain beverage I want
+        Then I add a bevarege in a cart
--- step-definitions/add-to-cart.js
+++ step-definitions/add-to-cart.js
@@ -1,9 +1,51 @@
-let app = require('../app.js');
+let myApp = require('../app.js');
 let ShoppingCart = require('../shopping-cart.js');
 let Category = require('../category.js');
+let Systembolaget = require('../systembolaget.js');
 
 module.exports = function(){
 
-	
+let store = new Systembolaget(20);
+let aPerson = new Person('Arnold', 25);
+let quantity = 1;
+
+ this.Given(/^that I'm in Systembolaget$/, function (callback) {
+ 	 
+ 	 if(store.people.includes(aPerson) == false) {
+ 		store.enter(aPerson);
+ 	 }
+           
+         // Write code here that turns the phrase above into concrete actions
+         callback();
+    
+       });
+ 
+ this.When(/^I have decided what I want to buy$/, function (callback) {
+ 	  product = myApp.products[62];
+ 	
+         // Write code here that turns the phrase above into concrete actions
+         callback();
+    
+       });
+
+	 this.When(/^I have decided how much of a certain beverage I want$/, function (callback) {
+	 	quantity = 1;
+    	
+    	
+         // Write code here that turns the phrase above into concrete actions
+         callback();
+    
+       });
+ this.Then(/^I add a bevarege in a cart$/, function (callback) {
+
+ 	   assert.doesNotThrow(function() {
+      aPerson.shoppingCart.add(product, quantity);
+    },
+ 	 "I can't add a beverage to the cart." ); 
+         // Write code here that turns the phrase above into concrete actions
+         callback();
+    
+       });
+
 
 }
