4bb9cde32e2a0902ab4e33217dd6793fccaf2145
Sergei <sergei.melnik90@gmail.com>
2018-04-27 10:35:56 +0200

    Fixes on add-to-cart feature

--- step-definitions/add-to-cart.js
+++ step-definitions/add-to-cart.js
@@ -1,53 +1,41 @@
-let App = require('../app.js');
-let Person = require('../person.js');
+let app = require('../app.js');
 let ShoppingCart = require('../shopping-cart.js');
 let Category = require('../category.js');
-let Systembolaget = require('../systembolaget.js');
+let assert = require('assert');
+let Person = require('../person.js');
 
 
 module.exports = function(){
 
-//let store = new Systembolaget(20);
-let aPerson = new Person('Arnold', 25);
-let quantity = 1;
+  let aPerson, product;
+  let quantity = 1;
 
- this.Given(/^that I'm in Systembolaget$/, function (callback) {
+  this.Given(/^that I'm in Systembolaget$/, function (callback) {
  	 
- 	 if(store.people.includes(aPerson) == false) {
- 		store.enter(aPerson);
- 	 }
-           
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-    
-       });
+ 	  if(app.users.includes("Arnold") == false) {
+ 		  aPerson = new Person('Arnold', 25);
+ 	  }
+    callback();
+  });
  
- this.When(/^I have decided what I want to buy$/, function (callback) {
+  this.When(/^I have decided what I want to buy$/, function (callback) {
  	  product = app.products[62];
- 	
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-    
-       });
+    callback();
+  });
 
-	 this.When(/^I have decided how much of a certain beverage I want$/, function (callback) {
+	this.When(/^I have decided how much of a certain beverage I want$/, function (callback) {
 	 	quantity = 1;
-    	
-    	
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-    
-       });
- this.Then(/^I add a bevarege in a cart$/, function (callback) {
+    callback();
+  });
 
- 	   assert.doesNotThrow(function() {
+  this.Then(/^I add a bevarege in a cart$/, function (callback) {
+ 	  assert.doesNotThrow(function() {
       aPerson.shoppingCart.add(product, quantity);
-    },
- 	 "I can't add a beverage to the cart." ); 
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-    
-       });
+      },
+ 	    "I can't add a beverage to the cart." 
+    ); 
+    callback();
+  });
 
 
 }
