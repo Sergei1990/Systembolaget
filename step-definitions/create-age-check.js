d6af3ca92573cb5eb937d187267f1e2cd1a07368
Unknown <jessica_nurmi@hotmail.com>
2018-05-04 09:48:17 +0200

    create-age .feature and .js

--- step-definitions/create-age-check.js
+++ step-definitions/create-age-check.js
@@ -3,52 +3,49 @@ module.exports = function(){
 let app = require('../app.js')
 let Person = require('../person.js');
 
-let age;
-let legalAge=20;
 
-
-this.Given(/^that a person is of legal age to buy alcohol$/, function (callback) {
-        user = app.addUser('Maria', 21);
-
-        //console.log(user)
-         callback();
-       });
-
-this.When(/^the person enter the onlinestore$/, function (callback) {
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-       });
-
-this.Then(/^an alert age\-check appears to click yes or no button if you are of legal age$/, function (callback) {
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-       });
-
-this.Then(/^the person click yes to enter the site$/, function (callback) {
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-       });
-
-
-//Scenari 2 (age 15) and scenario 3 (age 25)
-
-this.Given(/^that I am a registrered user with a known age of "([^"]*)"$/, function (arg1, callback) {
-         user = app.addUser('Maria', age);
-         callback();
-       });
-
- this.When(/^I try to see products on the web site$/, function (callback) {
-         products = app.products;
-         callback();
-       });
-
- this.Then(/^a warning should "([^"]*)" be displayed$/, function (arg1, callback) {
-         // Write code here that turns the phrase above into concrete actions
-         callback();
-       });
+let legalAge, underAge, person;
 
 
 
+this.Given(/^that a person want to buy alcohol$/, function (callback) {
+  legalAge = 20;
+  callback();
+});
+
+this.When(/^he enters name and legal age$/, function (callback) {
+  person = app.addUser("Klas", legalAge);
+  callback();
+});
+
+this.Then(/^he can see all the beverages$/, function (callback) {
+  assert(
+  app.products.length === 18695,
+  "User cant see all beverages"
+  );
+  callback();
+});
+
+
+
+
+this.Given(/^that a person want to buy non\-alcohol$/, function (callback) {
+  underAge = 18;
+  callback();
+});
+
+this.When(/^he enters name and age under (\d+)$/, function (arg1, callback) {
+  person = app.addUser("Klas", underAge);       
+  callback();
+});
+
+this.Then(/^he can only see the non\-alcoholic beverages$/, function (callback) {
+ assert(
+  app.products.length === 106,
+  "Under age users can see all the products"
+  );      
+  callback();
+});
 
 
 
