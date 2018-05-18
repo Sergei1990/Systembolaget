f464a1ca4c07499262566664413ef0d2bc20bfaa
Ekaterina_Fomenko <ekaterina.fomenko1709@gmail.com>
2018-05-18 22:18:33 +0200

    refactoring of AllaDrycker

--- www/js/allaDrycker.js
+++ www/js/allaDrycker.js
@@ -2,11 +2,16 @@
 class AllaDrycker {
 
 	constructor() {
+    
+	  	let user = app.addUser("Vasja", 17);//temporary
+
+
 
 		this.hideFilters();
 
 		// show the productlist "later" to prevent lag
 		setTimeout(()=>{
+			$('#productDescription').empty();
 			this.loadProducts();
 		}, 0);
 
@@ -18,11 +23,12 @@ class AllaDrycker {
 		$("#showMore").click((e)=>{
 			// Prevents a href from re-loading the website
 			e.preventDefault();
-			this.showMoreProducts();
+			//this.showMoreProducts();
+			this.loadProducts();
 	 	});
 
-	 	this.quantityOfProductOnPage;
-	 	this.quantityToShow = 2;	 	
+	 	this.quantityOfProductOnPage = 0;
+	 	this.quantityToShow = 50;	 	
 
 	}	
 
@@ -57,78 +63,45 @@ class AllaDrycker {
 	loadProducts(){
 	
 		let $div = $('#productDescription');
-	  	$div.empty();
-
-	  	let user = app.addUser("Vasja", 17);
-
-	  	let products = app.products.slice(0, this.quantityToShow); 
+	  	// $div.empty();
+	  	let products = app.products.slice(this.quantityOfProductOnPage); 
 	
-		for (let i = 0; i < products.length; i++) {
+		let qty;
+
+		if (products.length < this.quantityToShow)
+			qty = products.length;
+		else
+			qty = this.quantityToShow;
+
+		for (let i = 0; i < qty; i++) {
 			$div.append(
 				 '<div class="row pt-1 vertical-align">'
 				+   '<div class="prodPicture col-md-2">'
 				+       '<img src="www/img/11.jpg" alt="Picture">'
 				+   '</div>'
-				+   '<div id="prodName' + i + '" class="col-md-3">'
+				+   '<div id="prodName' + this.quantityOfProductOnPage + '" class="col-md-3">'
 				+       '<p>' + products[i].namn + '</p>'
 				+   '</div>'
-				+   '<div id="prodAlcohol' + i + '" class="col-md-2">'
+				+   '<div id="prodAlcohol' + this.quantityOfProductOnPage + '" class="col-md-2">'
 				+       '<p>' + products[i].alkoholhalt + '  %</p>'
 				+   '</div>'
-				+   '<div id="prodCountry' + i + '" class="col-md-2">'
+				+   '<div id="prodCountry' + this.quantityOfProductOnPage + '" class="col-md-2">'
 				+       '<p>' + products[i].ursprunglandnamn + '</p>'
 				+   '</div>'
-				+   '<div id="prodPrice' + i + '" class="col-md-1">'
+				+   '<div id="prodPrice' + this.quantityOfProductOnPage + '" class="col-md-1">'
 				+       '<p>' + products[i].prisinklmoms + '  SEK </p>'
 				+   '</div>'
 				+   '<div class="col-md-2 text-right">' 
-				+   	'<button id = "addButton' + i + '" class="btn btn-secondary my-2 my-sm-0" type="submit">Add</button>'   
+				+   	'<button id = "addButton' + this.quantityOfProductOnPage + '" class="btn btn-secondary my-2 my-sm-0" type="submit">Add</button>'   
 				+   '</div>'
 				+'</div>' //class="row"
 	           
 		    );
-		    this.quantityOfProductOnPage = i+1;		
+		    this.quantityOfProductOnPage++;
+		    if (app.products.length == this.quantityOfProductOnPage)
+				$("#element3").hide();		
 		}		
 	}// loadProducts()
-
-	showMoreProducts(){
-		let $div = $('#productDescription');
-		let products = app.products.slice(this.quantityOfProductOnPage);
-		
-		if (products.length < this.quantityToShow)
-			this.quantityToShow = products.length;
-
-		for (let i = 0; i < this.quantityToShow; i++) {
-			$div.append(
-				 '<div class="row pt-1 vertical-align">'
-				+   '<div class="prodPicture col-md-2">'
-				+       '<img src="www/img/11.jpg" alt="Picture">'
-				+   '</div>'
-				+   '<div id="prodName' + (this.quantityOfProductOnPage) + '" class="col-md-3">'
-				+       '<p>' + products[i].namn + '</p>'
-				+   '</div>'
-				+   '<div id="prodAlcohol' + (this.quantityOfProductOnPage) + '" class="col-md-2">'
-				+       '<p>' + products[i].alkoholhalt + '  %</p>'
-				+   '</div>'
-				+   '<div id="prodCountry' + (this.quantityOfProductOnPage) + '" class="col-md-2">'
-				+       '<p>' + products[i].ursprunglandnamn + '</p>'
-				+   '</div>'
-				+   '<div id="prodPrice' + (this.quantityOfProductOnPage) + '" class="col-md-1">'
-				+       '<p>' + products[i].prisinklmoms + '  SEK </p>'
-				+   '</div>'
-				+   '<div class="col-md-2 text-right">' 
-				+   	'<button id="addButton' + (this.quantityOfProductOnPage) + '" class="btn btn-secondary my-2 my-sm-0" type="submit">Add</button>'   
-				+   '</div>'
-				+'</div>' //class="row"
-	           
-		    );
-		    this.quantityOfProductOnPage = 1+this.quantityOfProductOnPage;		
-		} //for
-
-		if (app.products.length == this.quantityOfProductOnPage)
-			$("#element3").hide();
-
-	} //showMoreProducts()
 
 } //class
 
