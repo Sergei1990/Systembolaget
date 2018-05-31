2c3ead5663cf42433fa85129c8b01c593fa6c2f9
Sergei <sergei.melnik90@gmail.com>
2018-05-31 09:12:51 +0200

    Complete navbar selenium tests

--- step-definitions/s_navbar.js
+++ step-definitions/s_navbar.js
@@ -2,9 +2,12 @@ let assert = require('assert');
 
 module.exports = function(){
 
+  function sleep(ms = 1000) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 	this.Given(/^that I am on a startpage$/, async function() {
     return helpers.loadPage('http://localhost:3000/startpage.html').then(async function() {
-    	console.log('it works');
     });
   });
 
@@ -22,5 +25,38 @@ module.exports = function(){
 		);
 
   });
+
+  // 2nd Scenarion
+
+  this.Given(/^that I am on the mainpage$/, async function() {
+    return helpers.loadPage('http://localhost:3000/startpage.html').then(async function() {
+    });
+  });
+
+  this.When(/^I click on search field$/, async function() {
+    let searchField = await driver.findElement(by.css('#searchField'));
+    await searchField.click();
+  });
+
+  this.When(/^write corona in there$/, async function() {
+    let userInput = await driver.findElement(by.css('input#searchField'));
+    userInput.sendKeys('corona');
+  });
+
+  this.When(/^press search button$/, async function() {
+    let searchBotton = await driver.findElement(by.css('#searchButton'))
+  });
+
+  this.Then(/^I can see all corona products$/, async function() {
+    return helpers.loadPage('http://localhost:3000/AllaDrycker.html?searchinput=corona').then(async function() {
+    });
+    let body = await driver.findElement(by.css('body'));
+    let bodyText = await body.getText();
+    assert(
+      bodyText.includes('Corona Extra'),
+      "Couldn't find the text"
+    );
+  });
+
 
 }
